88e068d8cf3eb4c4a7e7a4e15221d23397b5085c
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-05-30 21:08:51 +0800

    依赖自动装配autowire byType会自己找bean然后转配上去

---
+++ Spring-07-di-autoware/src/main/java/com/wuxianggujun/spring/service/impl/BookServiceImpl.java
@@ -0,0 +1,20 @@
+package com.wuxianggujun.spring.service.impl;
+
+import com.wuxianggujun.spring.dao.BookDao;
+import com.wuxianggujun.spring.service.BookService;
+
+public class BookServiceImpl implements BookService {
+
+    private BookDao bookDao;
+
+    @Override
+    public void save() {
+        System.out.println("Book Service save ...");
+        bookDao.save();
+    }
+
+
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+}
