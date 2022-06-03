3ad51c5f90294c5be0c35fae29bf496fadd270f4
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-06-03 21:02:47 +0800

    不清楚视频里面的Mybatis库，主要是没有学过 Spring整合Junit

---
+++ Spring-15-spring-mybatis/src/main/java/com/wuxianggujun/spring/config/SpringConfig.java
@@ -0,0 +1,7 @@
+package com.wuxianggujun.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+}
---
+++ Spring-15-spring-mybatis/src/test/java/com/wuxianggujun/spring/service/AccountServiceTest.java
@@ -0,0 +1,12 @@
+package com.wuxianggujun.spring.service;
+
+import com.wuxianggujun.spring.config.SpringConfig;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+public class AccountServiceTest {
+
+}
