3d32ab254d14dec805eac640fd02eb3e624d27f0
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-06-03 20:39:45 +0800

    注解开发实现为第三方bean注入资源

--- Spring-14-annotation-third-bean-manager/src/main/java/com/wuxianggujun/spring/config/JdbcConfig.java
+++ Spring-14-annotation-third-bean-manager/src/main/java/com/wuxianggujun/spring/config/JdbcConfig.java
@@ -1,8 +1,9 @@
 package com.wuxianggujun.spring.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.wuxianggujun.spring.dao.BookDao;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
@@ -10,13 +11,23 @@ import javax.sql.DataSource;
 public class JdbcConfig {
     //定义一个方法获得要管理的对象
     //添加@Bean表示当前方法的返回值是一个bean
+    @Value("com.mysql.jdbc.Driver")
+    private String driver;
+    @Value("jdbc:mysql://localhost:3306/spring_db")
+    private String url;
+    @Value("root")
+    private String username;
+    @Value("root")
+    private String password;
+
     @Bean("dataSource")
-    public DataSource dataSource() {
+    public DataSource dataSource(BookDao bookDao) {
+        System.out.println(bookDao);
         DruidDataSource ds = new DruidDataSource();
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/spring_db");
-        ds.setUsername("root");
-        ds.setPassword("root");
+        ds.setDriverClassName(driver);
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
         return ds;
     }
 }
--- Spring-14-annotation-third-bean-manager/src/main/java/com/wuxianggujun/spring/config/SpringConfig.java
+++ Spring-14-annotation-third-bean-manager/src/main/java/com/wuxianggujun/spring/config/SpringConfig.java
@@ -1,10 +1,11 @@
 package com.wuxianggujun.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-//@ComponentScan("com.wuxianggujun.spring.config")
+@ComponentScan("com.wuxianggujun.spring.dao")
 @Import(JdbcConfig.class)
 public class SpringConfig {
 
